e819f1c50fcbf84f5ae5ad07af6041bb30fd07ab
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2025-02-17 15:32:50 -0300

    feat: Usando ORM para mapeamento de entidades

---
+++ src/main/java/br/com/backend/PsiRizerio/mapper/UserMapper.java
@@ -0,0 +1,42 @@
+package br.com.backend.PsiRizerio.mapper;
+
+import br.com.backend.PsiRizerio.model.UserDTO;
+import br.com.backend.PsiRizerio.persistence.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Named;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper
+@Component
+public class UserMapper {
+
+    @Named(value="toEntity")
+    public User toEntity(UserDTO model) {
+        User user = new User();
+        user.setName(model.getName());
+        user.setEmail(model.getEmail());
+        user.setPassword(model.getPassword());
+        user.setPhone(model.getPhone());
+        user.setAddress(model.getAddress());
+        user.setCpf(model.getCpf());
+        return user;
+    }
+
+    @Named(value="toDto")
+    public UserDTO toDto(User entity) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setName(entity.getName());
+        userDTO.setEmail(entity.getEmail());
+        userDTO.setPassword(entity.getPassword());
+        userDTO.setPhone(entity.getPhone());
+        userDTO.setAddress(entity.getAddress());
+        userDTO.setCpf(entity.getCpf());
+        return userDTO;
+    }
+
+    public List<UserDTO> toDto(List<User> users) {
+        return users.stream().map(this::toDto).toList();
+    }
+}
