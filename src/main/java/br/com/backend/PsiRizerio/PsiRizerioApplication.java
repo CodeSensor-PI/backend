eb059a4d5076060e4695d0ccbb0b3cbc378d79ed
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2025-02-16 22:03:45 -0300

    feat: Montando o Setup inicial da aplicação

---
+++ src/main/java/br/com/backend/PsiRizerio/PsiRizerioApplication.java
@@ -0,0 +1,13 @@
+package br.com.backend.PsiRizerio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiRizerioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsiRizerioApplication.class, args);
+	}
+
+}
