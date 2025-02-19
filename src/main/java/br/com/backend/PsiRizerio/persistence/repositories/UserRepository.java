1064c4cedc84d5509bd132b1efe56fd137af1b18
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2025-02-19 14:20:50 -0300

    feat: Repositorio para gerenciar dados da aplicação permitindo mapear objetos para tabelas de banco

--- src/main/java/br/com/backend/PsiRizerio/persistence/repositories/UserRepository.java
+++ src/main/java/br/com/backend/PsiRizerio/persistence/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import br.com.backend.PsiRizerio.persistence.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,5 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
     void deleteById(Long id);
     Optional<User> findById(Long id);
-    List<User> findAll();
+    Optional<User> findByEmail(String email);
+    Optional<User> findByCpf(String cpf);
+
 }
