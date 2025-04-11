27ba1f9eeb41ce36d56b085f92b3ce77d0985b00
KaueSilvaDev <kaue.o.silva35@gmail.com>
2025-04-10 23:03:49 -0300

    update: organizando atributos de acordo com frontend

--- src/main/java/br/com/backend/PsiRizerio/dto/usuarioDTO/UsuarioCreateDTO.java
+++ src/main/java/br/com/backend/PsiRizerio/dto/usuarioDTO/UsuarioCreateDTO.java
@@ -14,10 +14,6 @@ public class UsuarioCreateDTO {
     @NotBlank
     private String nome;
 
-    @CPF
-    @NotBlank
-    private String cpf;
-
     @Email
     @NotBlank
     private String email;
@@ -29,71 +25,5 @@ public class UsuarioCreateDTO {
 
     private PlanoResponseDTO fkPlano;
 
-    private EnderecoResponseDTO fkEndereco;
 
-    private LocalDateTime createdAt;
-
-    public @NotBlank String getNome() {
-        return nome;
-    }
-
-    public void setNome(@NotBlank String nome) {
-        this.nome = nome;
-    }
-
-    public @CPF @NotBlank String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(@CPF @NotBlank String cpf) {
-        this.cpf = cpf;
-    }
-
-    public @Email @NotBlank String getEmail() {
-        return email;
-    }
-
-    public StatusUsuario getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusUsuario status) {
-        this.status = status;
-    }
-
-    public void setEmail(@Email @NotBlank String email) {
-        this.email = email;
-    }
-
-    public @NotBlank String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(@NotBlank String senha) {
-        this.senha = senha;
-    }
-
-    public PlanoResponseDTO getFkPlano() {
-        return fkPlano;
-    }
-
-    public void setFkPlano(PlanoResponseDTO fkPlano) {
-        this.fkPlano = fkPlano;
-    }
-
-    public EnderecoResponseDTO getFkEndereco() {
-        return fkEndereco;
-    }
-
-    public void setFkEndereco(EnderecoResponseDTO fkEndereco) {
-        this.fkEndereco = fkEndereco;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 }
