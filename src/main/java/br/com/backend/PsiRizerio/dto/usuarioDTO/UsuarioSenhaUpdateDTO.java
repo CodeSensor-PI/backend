a9071779b6e4c3b7543315a1b0ae15b8fbaaae94
KaueSilvaDev <kaue.o.silva35@gmail.com>
2025-04-16 18:30:56 -0300

    refactor: adicionando lombok getter e setter

--- src/main/java/br/com/backend/PsiRizerio/dto/usuarioDTO/UsuarioSenhaUpdateDTO.java
+++ src/main/java/br/com/backend/PsiRizerio/dto/usuarioDTO/UsuarioSenhaUpdateDTO.java
@@ -1,27 +1,15 @@
 package br.com.backend.PsiRizerio.dto.usuarioDTO;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UsuarioSenhaUpdateDTO {
     @NotBlank
     private String senha;
 
     @NotBlank
     private String novaSenha;
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getNovaSenha() {
-        return novaSenha;
-    }
-
-    public void setNovaSenha(String novaSenha) {
-        this.novaSenha = novaSenha;
-    }
 }
