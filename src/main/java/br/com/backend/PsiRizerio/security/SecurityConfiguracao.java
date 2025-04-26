2befa8ce2e9594fd6e51d5c2d35b66b53bbaabdb
KaueSilvaDev <kaue.o.silva35@gmail.com>
2025-04-25 23:03:59 -0300

    update: usuarios -> pacientes

--- src/main/java/br/com/backend/PsiRizerio/security/SecurityConfiguracao.java
+++ src/main/java/br/com/backend/PsiRizerio/security/SecurityConfiguracao.java
@@ -53,11 +53,11 @@ public class SecurityConfiguracao {
             new AntPathRequestMatcher("/webjars/**"),
             new AntPathRequestMatcher("/v3/api-docs/**"),
             new AntPathRequestMatcher("/actuator/*"),
-            new AntPathRequestMatcher("/usuarios/login/**"),
+            new AntPathRequestMatcher("/pacientes/login/**"),
             new AntPathRequestMatcher("/h2-console/**"),
             new AntPathRequestMatcher("/h2-console/**/**"),
             new AntPathRequestMatcher("/error/**"),
-            new AntPathRequestMatcher("/usuarios/**"),
+            new AntPathRequestMatcher("/pacientes/**"),
             new AntPathRequestMatcher("/auth/**"),
     };
 
