4a409c5c67bf509f286e3ec18e591370af2678f8
KaueSilvaDev <kaue.o.silva35@gmail.com>
2025-04-27 17:35:10 -0300

    add: fkPlano

--- src/main/java/br/com/backend/PsiRizerio/dto/pacienteDTO/PacienteUpdateDTO.java
+++ src/main/java/br/com/backend/PsiRizerio/dto/pacienteDTO/PacienteUpdateDTO.java
@@ -2,6 +2,8 @@ package br.com.backend.PsiRizerio.dto.pacienteDTO;
 
 import br.com.backend.PsiRizerio.dto.enderecoDTO.EnderecoResponseDTO;
 import br.com.backend.PsiRizerio.dto.planoDTO.PlanoResponseDTO;
+import br.com.backend.PsiRizerio.dto.preferenciaDTO.PreferenciaResponseDTO;
+import br.com.backend.PsiRizerio.dto.telefoneDTO.TelefoneResponseDTO;
 import br.com.backend.PsiRizerio.enums.StatusUsuario;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -20,6 +22,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class PacienteUpdateDTO {
 
+    private PlanoResponseDTO fkPlano;
+
+    private StatusUsuario status = StatusUsuario.ATIVO;
+
     @NotBlank
     private String nome;
 
@@ -30,8 +36,6 @@ public class PacienteUpdateDTO {
     @Email
     @NotBlank
     private String email;
-
-    private StatusUsuario status = StatusUsuario.ATIVO;
 
     private EnderecoResponseDTO fkEndereco;
 
