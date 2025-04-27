74614ca6357e7daa60632ff4794599374f4b3b11
KaueSilvaDev <kaue.o.silva35@gmail.com>
2025-04-27 17:17:55 -0300

    add: findEndereco utilizando JPQL

--- src/main/java/br/com/backend/PsiRizerio/persistence/repositories/EnderecoRepository.java
+++ src/main/java/br/com/backend/PsiRizerio/persistence/repositories/EnderecoRepository.java
@@ -2,6 +2,8 @@ package br.com.backend.PsiRizerio.persistence.repositories;
 
 import br.com.backend.PsiRizerio.persistence.entities.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +16,23 @@ public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
 
     Endereco save(Endereco endereco);
 
-    boolean existsByCepAndBairroAndNumeroAndLogradouroAndUfIgnoreCase(String cep, String bairro, String numero, String logradouro, String uf);
+    @Query("""
+    SELECT CASE WHEN COUNT(e) > 0 THEN true ELSE false END
+    FROM Endereco e
+    WHERE 
+        e.cep = :cep AND
+        LOWER(e.logradouro) = LOWER(:logradouro) AND
+        e.numero = :numero AND
+        LOWER(e.cidade) = LOWER(:cidade) AND
+        LOWER(e.uf) = LOWER(:uf) AND
+        LOWER(e.bairro) = LOWER(:bairro)
+    """)
+    boolean findEndereco(
+            @Param("cep") String cep,
+            @Param("logradouro") String logradouro,
+            @Param("numero") String numero,
+            @Param("cidade") String cidade,
+            @Param("uf") String uf,
+            @Param("bairro") String bairro
+    );
 }
